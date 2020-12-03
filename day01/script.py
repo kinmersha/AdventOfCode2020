36c0aa6797b6aca1fbc956b4184e78ec4ba97645
Katerina <kinmersha@gmail.com>
2020-12-02 23:49:37 -0500

    day 1 part 2 solution

--- day01/script.py
+++ day01/script.py
@@ -11,6 +11,23 @@ def part1_lazy():
             if nums[a] + nums[b] == magic_num:
                 print(f'Part 1 answer: {nums[a]*nums[b]}')
                 return
+
+def part2_lazy():
+    import math
+    from itertools import combinations
+
+    with open('day01/input') as f:
+        nums = []
+        for line in f:
+            nums.append(int(line.strip()))
+
+    # Combinations of length 3
+    comb = combinations(nums, 3)
+    for c in comb:
+        if sum(c) == magic_num:
+            print(f'Part 2 answer: {math.prod(c)}')
+            return
     
 if __name__ == '__main__':
     part1_lazy()
+    part2_lazy()

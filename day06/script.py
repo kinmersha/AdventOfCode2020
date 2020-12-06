bc1d23c765be4e62110cb633b1bedb8e1b0fabd0
Katerina <kinmersha@gmail.com>
2020-12-06 00:44:13 -0500

    Complete day 6 part 2
    Sets are always fun :)

--- day06/script.py
+++ day06/script.py
@@ -1,4 +1,4 @@
-def read_data():
+def part1():
     with open('day06/input') as f:
         group_counts = []
         combo = ''
@@ -12,16 +12,32 @@ def read_data():
     # Get last group
     group_counts.append(set([c for c in combo]))
 
-    return group_counts
-
-
-def part1():
-    group_counts = read_data()
-
     print(f'Part 1 answer: {sum((len(s) for s in group_counts))}')
 
 
+# Even more set fun! Now we need to look at set intersections, which requires 
+# a modified version of the data-parsing, which is why I deleted the read_data()
+# function, since the code wasn't resuable.
+def part2():
+    with open('day06/input') as f:
+        group_counts = []
+        combo = ''
+        for line in f:
+            if line == '\n':
+                temp = [set((q for q in person)) for person in combo.split()]
+                # Use * 'splat' operator to get intersection of list of sets
+                group_counts.append(set.intersection(*temp))
+                combo = ''
+            else:
+                # Join on spaces to keep each line--each person--distinct.
+                combo = ' '.join([combo, line.strip()])
+    # Get last group
+    temp = [set((q for q in person)) for person in combo.split()]
+    group_counts.append(set.intersection(*temp))
+
+    print(f'Part 2 answer: {sum((len(s) for s in group_counts))}')
 
 
 if __name__ == '__main__':
     part1()
+    part2()
